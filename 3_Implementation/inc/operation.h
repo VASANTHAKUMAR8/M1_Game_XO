8994e94907780292642844593d9cfc95fbc7cd4c
VASANTHAKUMAR8 <94224310+VASANTHAKUMAR8@users.noreply.github.com>
2021-11-23 14:03:47 +0530

    Update operation.h

--- 3_Implementation/inc/operation.h
+++ 3_Implementation/inc/operation.h
@@ -1,47 +1,18 @@
-/** 
-* @file calculator_operations.h
-* Calculator application with 4 mathematical operations
-*
-*/
-#ifndef __CALCULATOR_OPERATIONS_H__
-#define __CALCULATOR_OPERATIONS_H__
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio_ext.h>
-
 /**
-*  adds the operand1 and operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 + operand2
-*/
-int add(int operand1, int operand2);
+ * @file operation.h
+ * @author your name 
+ * @brief Declaring the functions for user operations
+ * @version 0.1
+ * @date 2021-11-20
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef OPERATIONS_H /* IF NOT DEFINE THE OPERATIONS_H*/
 
-/**
-*  subtracts the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 - operand2
-*/
-int subtract(int operand1, int operand2);
-
-/**
-*  multiply the operand1, operand2 and returns the result
-* @param[in] operand1 
-* @param[in] operand2 
-* @return Result of operand1 * operand2
-*/
-int multiply(int operand1, int operand2);
-
-
-/**
-* divides the operand1 by operand2 and returns the result 
-* @param[in] operand1 
-* @param[in] operand2 
-* @return integer value of the operand1 / operand2
-* @note returns 0 for divide by 0 error
-*/
-int divide(int operand1, int operand2);
-
-#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
+#define OPERATIONS_H /* DEFINING THE OPERATIONS_H*/
+/* INCLUDING THE STRUCTURE_H */
+int display_board(char board[3][3]);
+int input(int number);
+int winner(char board[3][3]);
+#endif
