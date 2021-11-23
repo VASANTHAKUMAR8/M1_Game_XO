2a7243a03d7ec915805359d4ec4a3688a244059b
VASANTHAKUMAR8 <94224310+VASANTHAKUMAR8@users.noreply.github.com>
2021-11-23 11:01:13 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,144 +1,171 @@
-
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ncurses.h> 
 
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-int checkwin();
-void board();
-
-int main()
+int display_board(char board[3][3]); 
+int display_board(char board[3][3]) 
 {
+	int i = 0; // initialize loop counter
+	int j = 0; // initialize loop counter
+	
+	for (i = 0; i < 3; i++) 
+	{
+		printf(" \n ");
+		for (j = 0; j < 3; j++) 
+		{
+			printf(" %c ", board[i][j]); 
+		}
+	}
+	printf(" \n "); 
+	return 0;
+}
 
-    int player = 1, i, choice;
 
-    char mark;
-    do
-    {
-        board();
-        player = (player % 2) ? 1 : 2;
+int input(int number);
+int input(int number) 
+{
+	int row_or_column = 0; 
+	char* string_to_print[7]; 
+	if (number == 0) 
+	{
+		*string_to_print = "row";
+	} else if (number == 1) 
+	{
+		*string_to_print = "column";
+	} else {
+		printf("\n This is not supposed to hapen. \n"); 
+	}
+	int valid = 0; 
+	while (valid == 0)
+	{
+		printf("\n Choose a %s> ", *string_to_print); 
+		scanf("%d", &row_or_column); // Get input 
+		if (1 <= row_or_column && row_or_column <= 3)
+		{
+			printf("\n You have chosen %s #%d. \n", *string_to_print, row_or_column);
+			valid = 1;
+			
+		} else {
+			printf("\n %s = %d \n", *string_to_print, row_or_column); // Statement for debugging
+			printf("\n Invalid %s chosen, please try again. \n (The %s number must be either 1, 2, or 3.) \n", *string_to_print, *string_to_print);
+			
+		}
+	}
+	return row_or_column;
+}
 
-        printf("Player %d, enter a number:  ", player);
-        scanf("%d", &choice);
 
-        mark = (player == 1) ? 'X' : 'O';
+int winner(char board[3][3]);
+int winner(char board[3][3]) 
+{
+	int winner_number = 0;
+	int blank = 0;
+	int i = 0; 
+	int j = 0; 
+	if (((board[0][0] == 'X') && (board[0][1] == 'X') && (board[0][2] == 'X')) || // first row
+		((board[0][0] == 'X') && (board[1][0] == 'X') && (board[2][0] == 'X')) || // first column
+		((board[0][0] == 'X') && (board[1][1] == 'X') && (board[2][2] == 'X')) || // diagonal 1
+		((board[1][0] == 'X') && (board[1][1] == 'X') && (board[1][2] == 'X')) || // second row
+		((board[2][0] == 'X') && (board[2][1] == 'X') && (board[2][2] == 'X')) || // third row
+		((board[0][1] == 'X') && (board[1][1] == 'X') && (board[2][1] == 'X')) || // second column
+		((board[0][2] == 'X') && (board[1][2] == 'X') && (board[2][2] == 'X')) || // third column
+		((board[0][2] == 'X') && (board[1][1] == 'X') && (board[2][0] == 'X')) // diagonal 2
+		)
+	{
+		winner_number = 1;
+	} else if (((board[0][0] == 'O') && (board[0][1] == 'O') && (board[0][2] == 'O')) || // first row
+			   ((board[0][0] == 'O') && (board[1][0] == 'O') && (board[2][0] == 'O')) || // first column
+			   ((board[0][0] == 'O') && (board[1][1] == 'O') && (board[2][2] == 'O')) || // diagonal 1
+			   ((board[1][0] == 'O') && (board[1][1] == 'O') && (board[1][2] == 'O')) || // second row
+			   ((board[2][0] == 'O') && (board[2][1] == 'O') && (board[2][2] == 'O')) || // third row
+			   ((board[0][1] == 'O') && (board[1][1] == 'O') && (board[2][1] == 'O')) || // second column
+			   ((board[0][2] == 'O') && (board[1][2] == 'O') && (board[2][2] == 'O')) || // third column
+			   ((board[0][2] == 'O') && (board[1][1] == 'O') && (board[2][0] == 'O')) // diagonal 2
+			   )
+	{
+		winner_number = 2;
+	} else {
+		for (i = 0; i < 3; i++) 
+		{
+			for (j = 0; j < 3; j++) 
+			{
+				if (board[i][j] == '_') {
+					blank = 1; 
+				}
+			}
+		}
+		if (blank == 0) { 
+			winner_number = 3;
+		}
+	}
+	
+	return winner_number;
+}
 
-        if (choice == 1 && square[1] == '1')
-            square[1] = mark;
-            
-        else if (choice == 2 && square[2] == '2')
-            square[2] = mark;
-            
-        else if (choice == 3 && square[3] == '3')
-            square[3] = mark;
-            
-        else if (choice == 4 && square[4] == '4')
-            square[4] = mark;
-            
-        else if (choice == 5 && square[5] == '5')
-            square[5] = mark;
-            
-        else if (choice == 6 && square[6] == '6')
-            square[6] = mark;
-            
-        else if (choice == 7 && square[7] == '7')
-            square[7] = mark;
-            
-        else if (choice == 8 && square[8] == '8')
-            square[8] = mark;
-            
-        else if (choice == 9 && square[9] == '9')
-            square[9] = mark;
-            
-        else
-        {
-            printf("Invalid move ");
 
-            player--;
-            getch();
-        }
-        i = checkwin();
-
-        player++;
-    }while (i ==  - 1);
-    
-    board();
-    
-    if (i == 1)
-        printf("==>\aPlayer %d win ", --player);
-    else
-        printf("==>\aGame draw");
-
-    getch();
-
+int main (int argc, const char * argv[]) { 
+	int number = 0; 
+	int player = 1; 
+	int flag = 0; 
+	printf("\n Message for security and/or debugging: \n This program's path is %s and it is running with %i argument(s). \n", argv[0], argc); 
+	char board[3][3] = {{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}}; 
+	int won = 0; 
+	// printf("\n won = %i \n", won); 
+	printf("\n Here is a blank Tic-Tac-Toe board for you and a friend to play Tic-Tac-Toe on: \n");
+	display_board(board); 
+	while (won == 0) 
+	{
+		while (flag == 0) {
+			printf("\n It is player #%i's turn. \n", player); 
+			int row = input(number); 
+			row--; 
+			
+			number++; 
+			int column = input(number);
+			column--;  
+			
+			if (player == 1 && board[row][column] == '_') {
+				board[row][column] = 'X';
+				
+				flag = 1;
+			} else if (player == 2 && board[row][column] == '_') {
+				board[row][column] = 'O';
+				
+				flag = 2;
+			} else if (board[row][column] == 'O') {
+				printf("\n That space is taken by O. Please try again. \n");
+			} else if (board[row][column] == 'X') {
+				printf("\n That space is taken by X. Please try again. \n");
+			} else {
+				printf("This isn't supposed to happen"); 
+				exit(1);
+			}
+			
+			number = 0; // Reset
+		}
+		printf("\n The board now looks like this: \n");
+		display_board(board); 
+		won = winner(board); 
+		
+		if (won == 1) { 
+			printf("\n Player 1 wins! \n");
+		} else if (won == 2) { 
+			printf("\n Player 2 wins! \n");
+		} else if (won == 3) { 
+			printf("\n It's a tie. \n");
+		} else {
+			flag = 0; 
+			won = 0;  
+		}
+		if (player == 1) { 
+			player = 2; 
+		} else if (player == 2) {
+			player = 1;
+		} else {
+			printf("This isn't supposed to happen"); 
+		}
+	}
+	printf(" \n "); 
     return 0;
-
-}
-
-
-
-int checkwin()
-{
-    if (square[1] == square[2] && square[2] == square[3])
-        return 1;
-        
-    else if (square[4] == square[5] && square[5] == square[6])
-        return 1;
-        
-    else if (square[7] == square[8] && square[8] == square[9])
-        return 1;
-        
-    else if (square[1] == square[4] && square[4] == square[7])
-        return 1;
-        
-    else if (square[2] == square[5] && square[5] == square[8])
-        return 1;
-        
-    else if (square[3] == square[6] && square[6] == square[9])
-        return 1;
-        
-    else if (square[1] == square[5] && square[5] == square[9])
-        return 1;
-        
-    else if (square[3] == square[5] && square[5] == square[7])
-        return 1;
-        
-    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
-        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
-        != '7' && square[8] != '8' && square[9] != '9')
-
-        return 0;
-    else
-        return  - 1;
-
-        
-
-}
-
-
-
-
-
-void board()
-{
-    system("cls");
-    printf("\n\n\tTic Tac Toe\n\n");
-
-    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
-
-
-    printf("     |     |     \n");
-    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
-
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-
-    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
-
-    printf("_____|_____|_____\n");
-    printf("     |     |     \n");
-
-    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
-
-    printf("     |     |     \n\n");
 }
