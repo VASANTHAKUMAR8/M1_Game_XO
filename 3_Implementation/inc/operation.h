36040703c09979ee121441eecdb37f3a8359eaf2
VASANTHAKUMAR8 <94224310+VASANTHAKUMAR8@users.noreply.github.com>
2021-11-25 00:23:25 +0530

    Update operation.h

--- 3_Implementation/inc/operation.h
+++ 3_Implementation/inc/operation.h
@@ -8,9 +8,9 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#ifndef OPERATIONS_H /* IF NOT DEFINE THE OPERATIONS_H*/
+#ifndef OPERATION_H /* IF NOT DEFINE THE OPERATIONS_H*/
 
-#define OPERATIONS_H /* DEFINING THE OPERATIONS_H*/
+#define OPERATION_H /* DEFINING THE OPERATIONS_H*/
 /* INCLUDING THE STRUCTURE_H */
 int display_board(char board[3][3]);
 int input(int number);
